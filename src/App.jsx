530c042f2a781dedbc45837b49b02679373e138e
razumolla <razumolla75@gmail.com>
2024-12-05 18:31:33 +0600

    tailwind and daisyui

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 function App() {
   return (
     <>
-      <div>This Razu Molla</div>
+      <h1 className="text-3xl font-bold underline ">Hello world!</h1>
+      <p className="text-3xl">This is a React app</p>
+
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button>
     </>
   );
 }
