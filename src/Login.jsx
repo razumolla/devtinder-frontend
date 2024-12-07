97c25317342fa35ce42285301439fd8a7ee005de
razumolla <razumolla75@gmail.com>
2024-12-07 16:26:44 +0600

    login post

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,5 +1,68 @@
+import axios from "axios";
+import { useState } from "react";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [emailId, setEmailId] = useState("santo@gmail.com");
+  const [password, setPassword] = useState("Razu@12345");
+
+  const handleSubmit = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:5000/login",
+        {
+          emailId,
+          password,
+        },
+        { withCredentials: true }
+      );
+
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="flex justify-center my-10">
+      <div className="card bg-base-300 w-96 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <div className="mb-4">
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Email Id</span>
+              </div>
+              <input
+                type="text"
+                value={emailId}
+                onChange={(e) => setEmailId(e.target.value)}
+                placeholder="Enter Your Email Id"
+                className="input input-bordered w-full max-w-xs"
+              />
+            </label>
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Password </span>
+              </div>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Enter Your Password"
+                className="input input-bordered w-full max-w-xs"
+              />
+            </label>
+          </div>
+
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary" onClick={handleSubmit}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
